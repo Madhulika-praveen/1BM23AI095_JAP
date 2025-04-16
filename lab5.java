ff26c76ec373889ddb70fab58b7473834f0cd2e3
Madhulika-praveen <madhulika.ai23@bmsce.ac.in>
2025-04-16 15:05:44 +0530

    Add files via upload

---
+++ lab5.java
@@ -0,0 +1,71 @@
+package mp;
+import java.util.Vector;
+
+public class lab5 {
+
+    class Product {
+        String name;
+        double price;
+        String category;
+
+        public Product(String name, double price, String category) {
+            this.name = name;
+            this.price = price;
+            this.category = category;
+        }
+    }
+
+    class OnlineShoppingManager {
+        private Vector<Product> inventory;
+
+        public OnlineShoppingManager() {
+            inventory = new Vector<>();
+        }
+
+        public void addProduct(String name, double price, String category) {
+            Product product = new Product(name, price, category);
+            inventory.add(product);
+        }
+
+        public void removeProduct(String name) {
+            for (int i = 0; i < inventory.size(); i++) {
+                if (inventory.get(i).name.equalsIgnoreCase(name)) {
+                    inventory.remove(i);
+                    break;
+                }
+            }
+        }
+
+        public void displayInventory() {
+            System.out.println("Inventory:");
+            for (Product product : inventory) {
+                System.out.println("Name: " + product.name + ", Price: " + product.price + ", Category: " + product.category);
+            }
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        lab5.OnlineShoppingManager manager = new lab5().new OnlineShoppingManager();
+
+        // Add some products to the inventory
+        manager.addProduct("Laptop", 40000, "Electronics");
+        manager.addProduct("T-shirt", 550, "Clothing");
+        manager.addProduct("Book", 920, "Books");
+
+        // Display all products in the inventory
+        manager.displayInventory();
+
+        // Remove a product
+        manager.removeProduct("Book");
+
+        // Display updated inventory
+        manager.displayInventory();
+
+        manager.removeProduct("Laptop");
+
+        // Display updated inventory
+        manager.displayInventory();
+    }
+}
